68d666f1859597e33339d7e0117e7bf3b2d4b1d6
DevDK <kolyadin.2007@ya.ru>
2022-01-03 22:23:00 +0500

    Добавленно создание папок и перемещение файлов

--- main.py
+++ main.py
@@ -205,6 +205,49 @@ def dlg_dismiss (dlg, new_name_dlg):
 	htyjtyjtyjytyjyj = new_name_dlg.get()
 	dlg.grab_release()
 	dlg.destroy()
+def dlg_cr_dir():
+	global htyjtyjtyjytyjyj
+	dlg = Toplevel(root)
+
+
+	w = root.winfo_width()
+	h = root.winfo_height()
+
+	stgg = 0
+	xcord = ''
+	ycord = ''
+	for kjkd in root.geometry():
+		if kjkd == '+':
+			stgg = stgg +1
+		elif stgg==1:
+			xcord = xcord + kjkd
+		elif stgg==2:
+			ycord = ycord + kjkd
+		else:
+			pass
+	xcord=int(xcord)
+	ycord=int(ycord)
+	dlg.title('Cоздать директорию...')
+	Label(dlg, text='Введите имя директории:').grid()
+	new_name_dlg = Entry(dlg)
+	new_name_dlg.grid()
+	ttk.Button(dlg, text="Создать", command= lambda: dlg_dismiss(dlg, new_name_dlg)).grid()
+	w = w//2
+	h = h//2
+	w1 = xcord + w
+	w2 = ycord + h
+
+	dlg.transient(root)   # dialog window is related to main
+	dlg.wait_visibility() # can't grab until window appears, so we wait
+	dlg.grab_set()        # ensure all input goes to our window
+
+	w11 = dlg.winfo_width() //2
+	w21 = dlg.winfo_height() //2
+	dlg.geometry('+{}+{}'.format(w1 - w11, w2 - w21))
+
+	dlg.protocol("WM_DELETE_WINDOW", lambda:  dlg_dismiss(dlg, new_name_dlg)) # intercept close button
+	dlg.wait_window()     # block until window is destroyed
+	return htyjtyjtyjytyjyj
 
 def dlg_rename():
 	global htyjtyjtyjytyjyj
@@ -418,8 +461,28 @@ def info_f():
 #
 def replace_f():
 	if login_bas:
-		print('an_auth')
-	alert('Недоступно, обращайтесь к @GunsForHand_s')
+		print('replace_f')
+	if os.path.isfile(tree.item(item_select)['values'][0] + '/' + tree.item(item_select)['text']):
+		alert('Выберите в какую папку переместить файл')
+		dir = filedialog.askdirectory(mustexist=1, title='Куда переместить файл')
+		os.replace(tree.item(item_select)['values'][0] + '/' + tree.item(item_select)['text'], dir+ '/' + tree.item(item_select)['text'])
+
+		old_path = tree.item(item_select)['values'][0] + '/' + tree.item(item_select)['text']
+		old_dir = tree.item(item_select)['values'][0] + '/'
+		iterdorsss = 0
+		for vremen in settings['recent_use']:
+			if vremen==old_path:
+				settings['recent_use'][iterdorsss] = dir +'/' + tree.item(item_select)['text']
+			iterdorsss = iterdorsss + 1
+
+		save_settings()
+		f_name =tree.item(item_select)['text']
+		tree.delete(item_select)
+		rec = dir +'/' + f_name
+		rtee.append(tree.insert('', 'end', text=os.path.basename(rec), values=(f_this_space(os.path.dirname(rec)))))
+	else:
+		alert('Файл был удалён или перемещён')
+
 
 #
 def create_f():
@@ -441,9 +504,13 @@ def create_f():
 #
 def create_dir():
 	if login_bas:
-		print('an_auth')
-	alert('Недоступно, обращайтесь к @GunsForHand_s')
-	print(root.geometry())
+		print('create_dir')
+	alert('Выберите расположение новой директории')
+	dir_path = filedialog.askdirectory(mustexist=False, title='Выберите расположение новой директории')
+	dirname = dlg_cr_dir()
+	os.mkdir(dir_path+'/' +dirname)
+	alert('Директория созданна - '+dir_path+'/'+dirname)
+
 
 #
 def an_auth():
